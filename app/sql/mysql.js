630686898b3f9d17d3c3efce3191aaaadde9bb11
jermken <jermken@163.com>
2018-10-11 17:45:08 +0800

    '修复sql语法错误'

--- app/sql/mysql.js
+++ app/sql/mysql.js
@@ -22,6 +22,10 @@ let query = (sql, values) => {
     })
 }
 
+/** 
+ * 用户表
+ * id  唯一标志
+*/
 let fUsers = 
     `create table if not exists fusers(
         id INT NOT NULL AUTO_INCREMENT,
@@ -31,11 +35,43 @@ let fUsers =
         update_time VARCHAR(100) NOT NULL COMMENT '修改时间',
         PRIMARY KEY(id)
     );`
+    
+/** 
+ * 客户表
+ * id  唯一标志
+*/
+let Users = 
+    `create table if not exists users(
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL COMMENT '客户名',
+        sexual VARCHAR(100) NOT NULL COMMENT '性别',
+        PRIMARY KEY(id)
+    );`
 
+/** 
+ * 流水表
+ * id  唯一标志
+*/
+let Bills = 
+    `create table if not exists bills(
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL COMMENT '客户名',
+        date VARCHAR(100) NOT NULL COMMENT '消费日期',
+        project VARCHAR(100) NOT NULL COMMENT '消费项目',
+        total VARCHAR(100) NOT NULL COMMENT '消费总额',
+        card_minu VARCHAR(100) NOT NULL COMMENT '扣卡金额',
+        pay_amount VARCHAR(100) NOT NULL COMMENT '收纳金额',
+        no_pay VARCHAR(100) NOT NULL COMMENT '未付金额',
+        pay_type VARCHAR(100) NOT NULL COMMENT '付款方式',
+        remark VARCHAR(100) NOT NULL COMMENT '备注',
+        PRIMARY KEY(id)
+    );`
 let createTable = (sql) => {
     return query(sql, [])
 }
 // 建表
 createTable(fUsers)
+createTable(Users)
+createTable(Bills)
 
 exports.query = query
